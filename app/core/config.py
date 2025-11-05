0de8d84d7ff819d061917bfe4cfe241a2bd81feb
madietran <125259864+madietran@users.noreply.github.com>
2025-11-06 01:33:28 +0700

    add: access token constants

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from typing import List
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+from typing import Literal, Optional
 
 load_dotenv()
 
@@ -37,7 +38,14 @@ class Configs(BaseSettings):
     DATE_FORMAT: str = '%d-%m-%Y'
 
     SECRET_KEY: str = os.getenv('SECRET_KEY', '')
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 60 * 2)
+    ACCESS_TOKEN_MAX_AGE: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 60 * 2)
+    REFRESH_TOKEN_MAX_AGE: int = 2592000      # 30d
+    COOKIE_SECURE: bool = False               # True in prod (HTTPS)
+    COOKIE_SAMESITE: Literal["lax","strict","none"] = "lax"
+    COOKIE_DOMAIN: Optional[str] = None       # e.g. .example.com
+    COOKIE_PATH: str = "/"
+    ACCESS_TOKEN_NAME: str = "access_token"
+    REFRESH_TOKEN_NAME: str = "refresh_token"
 
     BACKEND_CORS_ORIGINS: List[str] = ['*']
 
