ccb8206d609ca6b5d4c6ab690866d00bc806b1ad
tuannhat <altn2510@gmail.com>
2025-11-08 18:30:36 +0700

    [FIX]: add bearer auth into swagger ui

--- app/core/dependencies/auth_deps.py
+++ app/core/dependencies/auth_deps.py
@@ -1,6 +1,9 @@
 from typing import Optional
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
+)
 from dependency_injector.wiring import inject, Provide
 
 from app.core.containers.application_container import ApplicationContainer
@@ -8,12 +11,14 @@ from app.db.databases.supabase import SupabaseDatabase
 from app.repositories.user_repository import UserRepository
 from app.schemas.user_schema import UserResponse
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
+# auto_error=False to allow optional authentication
+bearer_scheme = HTTPBearer(auto_error=False)
 
 
 @inject
 def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    # token: str = Depends(oauth2_scheme),
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
     supabase_db: SupabaseDatabase = Depends(
         Provide[ApplicationContainer.database.supabase_db]
     ),
@@ -24,6 +29,12 @@ def get_current_user(
     '''Dependency returns authenticated user from Supabase token.'''
     try:
         sb = supabase_db.get_client()
+        token = credentials.credentials if credentials else None
+        if not token:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Not authenticated',
+            )
 
         # 1. Authenticate token with Supabase Auth
         auth_res = sb.auth.get_user(token)
